4ff93b4d0879c96d72010e36b7bb4388271ff0f8
Pmob.noo <t4ng1336@gmail.com>
2021-10-07 11:19:44 +0700

    Update Excercise4_Pomboon_P.py

--- Excercise4_Pomboon_P.py
+++ Excercise4_Pomboon_P.py
@@ -1,1 +1,9 @@
 English = 100
+Business = 80
+Intro_computerSystem = 90
+Programming = 100
+print("--- Your Score ---")
+print("Foundation English :",English)
+print("General Business :",Business)
+print("Introduction to Computer Systems :",Intro_computerSystem)
+print("Computer Programming :",Programming)
